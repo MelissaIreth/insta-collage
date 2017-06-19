047641e0915e67d2e33c1d1ca8d456b8bd01c5af
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-06-18 20:05:16 -0400

    collage and validation

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/*DRAG DROP*/
+
+function drag(ev) {
+  console.log(ev.target.src);
+  ev.dataTransfer.setData('text', ev.target.id);
+}
+
+function permitirDrop(ev){
+  ev.preventDefault();
+} 
+
+function drop(ev) {
+  ev.preventDefault();
+  var id_foto = ev.dataTransfer.getData('text');
+  ev.target.appendChild(document.getElementById(id_foto));
+}
+
+/*VALIDACION PARA IR A COLLAGE*/
+
+
+function toCollage(){
+	var user = document.getElementById("user").value;
+	var pass = document.getElementById("pass").value;
+
+	var btnSesion = document.getElementsByClassName("btn-sesion")[0];
+
+
+	if(user==""){
+		var errorU = document.createElement("span")
+		errorU.classList.add("error-u")
+		var textU = document.createTextNode("Ingrese Usuario")
+
+		var userbien = document.getElementsByClassName("form-group")[0];
+		errorU.appendChild(textU)
+		userbien.appendChild(errorU)
+	
+	}else{
+		window.location.href="index2.html"
+	}
+
+	if((pass=="") || (pass==123456) || (pass.length<5) || (pass.length>7)){
+		var errorP = document.createElement("span")
+		errorP.classList.add("error-p")
+		var textP = document.createTextNode("Ingrese Contraseña")
+
+		var passbien = document.getElementsByClassName("form-group")[0];
+		errorP.appendChild(textP)
+		passbien.appendChild(errorP)
+
+	}else{
+		window.location.href="index2.html"
+	}
+}
+
+var button = document.getElementById("boton");
+button.addEventListener("click", toCollage);
